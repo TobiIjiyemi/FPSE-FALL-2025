3df700f89c12e6ad066737a49e8778605eafb71f
Tobi Ijiyemi <TobiIjiyemi@gmail.com>
2025-09-21 14:44:46 -0400

    remove log files sorry.

--- assignment3/_build/default/src/dict_item.ml
+++
@@ -1,8 +0,0 @@
-
-open Core
-
-type 'a t = { key : string ; value : 'a } [@@deriving show]
-
-(* This is implemented for you *)
-let compare (x : 'a t) (y : 'a t) : int =
-  String.compare x.key y.key
